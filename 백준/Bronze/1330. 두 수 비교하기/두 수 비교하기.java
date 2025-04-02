31dfc5469a3ab08daf190ae89a6d03834c93e34a
jihyun <jhleezz00@naver.com>
2025-04-02 12:22:21 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        int firstNum = s.nextInt();
+        int secondNum = s.nextInt();
+        
+        if (firstNum > secondNum) {
+            System.out.println(">");
+        } else if (firstNum < secondNum) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
