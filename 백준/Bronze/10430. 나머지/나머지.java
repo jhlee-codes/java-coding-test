c40abeb9e2c33b3348528a96f3c6ca0a30a424b7
jihyun <jhleezz00@naver.com>
2025-04-02 13:15:23 +0900

    [Bronze V] Title: 나머지, Time: 180 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        s.close();
+        
+        System.out.println((a+b)%c);
+        System.out.println(((a%c) + (b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c) * (b%c))%c);  
+
+    }
+}
