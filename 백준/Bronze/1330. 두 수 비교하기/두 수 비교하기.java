5fea890560366037dff3fd66121544c1d52a6362
jihyun <jhleezz00@naver.com>
2025-04-02 12:38:43 +0900

    Update 두 수 비교하기.java

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        
         int firstNum = s.nextInt();
         int secondNum = s.nextInt();
+        s.close();
         
         if (firstNum > secondNum) {
             System.out.println(">");
