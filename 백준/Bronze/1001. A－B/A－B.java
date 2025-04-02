83e6217cebc8b126e7aafdcb60a7076a87115fb1
jihyun <jhleezz00@naver.com>
2025-04-02 09:00:08 +0900

    [Bronze V] Title: A-B, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int firstNum = s.nextInt();
+        int secondNum = s.nextInt();
+        System.out.println(firstNum-secondNum);
+    }
+}
