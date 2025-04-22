bc30e5a27c19d9a9e8a3dd3de7e1debfa52f52cf
jihyun <jhleezz00@naver.com>
2025-04-22 21:38:33 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1792 ms, Memory: 191004 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        int[][] coordinate = new int[num][2];
+        for (int i=0;i<num;i++) {
+            coordinate[i][0] = s.nextInt();
+            coordinate[i][1] = s.nextInt();
+        }
+        s.close();
+
+        Arrays.sort(coordinate, (o1,o2) ->
+                (o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1]));
+
+        for (int[] arr: coordinate) {
+            System.out.println(arr[0]+ " "+ arr[1]);
+        }
+    }
+}
