dbc18c0e981c301484ab89666b10d5ebbaa37a73
jihyun <jhleezz00@naver.com>
2025-08-26 06:55:10 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 50432 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    // 문제 : N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내기
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        // 시간초과 -> HashSet 이용
+        HashSet<Integer> nums = new HashSet<Integer>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            int result = nums.contains(target) ? 1 : 0;
+            sb.append(result).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
