cdce310739358c401371b73c4f206e052a4307d7
jihyun <jhleezz00@naver.com>
2025-12-10 19:13:03 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 148 ms, Memory: 14844 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -2,35 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int num = Integer.parseInt(st.nextToken());
-        int target = Integer.parseInt(st.nextToken());
-        Queue<Integer> queue = new LinkedList<>();
         List<Integer> list = new ArrayList<>();
-        for (int i=0;i<num;i++) {
-            queue.add(i+1);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(st.nextToken());
+        int k  = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new ArrayDeque<>();
+
+        // 큐 초기화
+        for (int i = 0; i < n; i++) {
+            q.add(i + 1);
         }
 
-        while (!queue.isEmpty()) {
-            for (int i=1;i<target;i++) {
-                queue.add(queue.poll());
+        while (!q.isEmpty()) {
+
+            // 앞에서부터 뺀 숫자를 뒤에 다시 삽입 (원형 큐)
+            for (int j = 0; j < k - 1; j++) {
+                q.add(q.poll());
             }
-            list.add(queue.poll());
+
+            list.add(q.poll());
         }
+
         bw.write("<");
-        for (int i=0;i<list.size();i++) {
-            bw.write(String.valueOf(list.get(i)));
-            if (i<list.size()-1) {
+
+        for (int i = 0; i < list.size(); i++) {
+            bw.write(list.get(i).toString());
+            if (i != list.size() - 1) {
                 bw.write(", ");
             }
-
         }
+
         bw.write(">");
+
         bw.flush();
-        bw.close();
     }
 }
