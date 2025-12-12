4c7c6576c66e2a1d6a453581dce606c11256a039
jihyun <jhleezz00@naver.com>
2025-12-13 02:04:18 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -5,24 +5,17 @@ class Solution {
         int[] answer = new int[2];
         // 블록 전체 개수 = brown + yellow
         int total = brown + yellow;
-        int width, height;
         
-        // 약수 구하기
-        List<Integer> divisorList = new ArrayList<>();
-        for (int i = 1; i <= total; i++) {
-            if (total % i == 0) {
-                divisorList.add(i);
-            }
-        }
-        
-        for (int i = 0; i < divisorList.size(); i++) {
-            height = divisorList.get(i);
-            width = total / height;
+        for (int w = 3; w < total; w++) {
+            
+            if (total % w != 0) continue;
+            int h = total / w;
             
             // yellow = (width - 2) * (height - 2)
-            if (yellow == (width - 2) * (height - 2)) {
-                answer[0] = width;
-                answer[1] = height;
+            if (yellow == (w - 2) * (h - 2)) {
+                // width >= height
+                answer[0] = Math.max(w, h);
+                answer[1] = Math.min(w, h);
                 break;
             }
         }
