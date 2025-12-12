eda816ae119a5ee44db6c4957db145efd2daede8
jihyun <jhleezz00@naver.com>
2025-12-12 16:56:04 +0900

    [level 2] Title: 오픈채팅방, Time: 148.66 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -5,28 +5,31 @@ class Solution {
         List<String> answer = new ArrayList<>();
         
         HashMap<String, String> userMap = new HashMap<>();
-        String id, nickName;
         
         for (int i = 0; i < record.length; i++) {
-            if (record[i].startsWith("Enter") || record[i].startsWith("Change")) {
-                id = record[i].split(" ")[1];
-                nickName = record[i].split(" ")[2];
+            String id, nickName;
+            String[] parts = record[i].split(" ");
+            
+            if (parts[0].equals("Enter") || parts[0].equals("Change")) {
+                id = parts[1];
+                nickName = parts[2];
                 userMap.put(id, nickName);
             }
         }
         
         for (int i = 0; i < record.length; i++) {
-            
-            id = record[i].split(" ")[1];
+            String id, nickName;
+            String[] parts = record[i].split(" ");
+            id = parts[1];
             nickName = userMap.get(id);
             
-            if (record[i].startsWith("Enter")) {
+            if (parts[0].equals("Enter")) {
                 answer.add(nickName + "님이 들어왔습니다.");
-            } else if (record[i].startsWith("Leave")) {
+            } else if (parts[0].equals("Leave")) {
                 answer.add(nickName + "님이 나갔습니다.");
             }
         }
         
-        return answer.toArray(new String[answer.size()]);
+        return answer.toArray(new String[0]);
     }
 }
