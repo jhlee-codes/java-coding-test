9c7a64e48b43bd2c9f17ba3eece0098253882079
jihyun <jhleezz00@naver.com>
2026-01-17 16:39:01 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,40 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "Yes";
-        
-        Queue<String> cards1Q = new ArrayDeque<>();
-        Queue<String> cards2Q = new ArrayDeque<>();
-        Queue<String> goalQ = new ArrayDeque<>();
+        ArrayDeque<String> cards1Q = new ArrayDeque<>();
+        ArrayDeque<String> cards2Q = new ArrayDeque<>();
         
         for (int i = 0; i < cards1.length; i++) {
-            cards1Q.add(cards1[i]);
+            cards1Q.addLast(cards1[i]);
         }
-        
         for (int i = 0; i < cards2.length; i++) {
-            cards2Q.add(cards2[i]);
+            cards2Q.addLast(cards2[i]);
         }
         
         for (int i = 0; i < goal.length; i++) {
-            goalQ.add(goal[i]);
+            String target1 = cards1Q.peekFirst();
+            String target2 = cards2Q.peekFirst();
+            if (goal[i].equals(target1)) cards1Q.pollFirst();
+            else if (goal[i].equals(target2)) cards2Q.pollFirst();
+            else return "No";
         }
         
-        while(!goalQ.isEmpty()) {
-            
-            String target = goalQ.peek();
-            
-            if (target.equals(cards1Q.peek())) {
-                cards1Q.poll();
-                goalQ.poll();
-            } else if (target.equals(cards2Q.peek())) {
-                cards2Q.poll();
-                goalQ.poll();
-            } else {
-                answer = "No";
-                break;
-            }
-        }
-        
-        return answer;
+        return "Yes";
     }
 }
