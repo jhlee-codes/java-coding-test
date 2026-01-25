6b6ff54225efeb87c84988c2a6da1c80619661c3
jihyun <jhleezz00@naver.com>
2026-01-26 01:19:27 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 67.22 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,42 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        
-        // {추천 대상 : 추천인}
-        Map<String, String> parentMap = new HashMap<>();
+        // {판매원 : 추천인}
+        Map<String, String> referralMap = new HashMap<>();
         // {판매원 : 이익금}
         Map<String, Integer> profitMap = new HashMap<>();
         
         for (int i = 0; i < enroll.length; i++) {
-            String parent = referral[i];
-            String child = enroll[i];
-            
-            parentMap.put(child, parent);
+            referralMap.put(enroll[i], referral[i]);
+            profitMap.put(enroll[i], 0);
         }
         
         for (int i = 0; i < seller.length; i++) {
             String target = seller[i];
             int total = amount[i] * 100;
             
+            // 판매 이익 분배 (루트 방향)
             while (!target.equals("-") && total > 0) {
-                int pass = total / 10;
-                int keep = total - pass;
+                int margin = total / 10;    // 추천인 이익 (10%)
+                int keep = total - margin;  // 본인 판매 이익 (90%)
                 
                 profitMap.put(target, profitMap.getOrDefault(target, 0) + keep);
                 
-                String refTarget = parentMap.get(target);
-                
-                target = refTarget;
-                total = pass;
+                // 부모 노드로 이동 (추천인)
+                target = referralMap.get(target);
+                total = margin;
             }
         }
-    
-        int[] answer = new int[enroll.length];
+        
+        int[] result = new int[enroll.length];
         
         for (int i = 0; i < enroll.length; i++) {
-            answer[i] = profitMap.getOrDefault(enroll[i], 0);
+            result[i] = profitMap.get(enroll[i]);
         }
         
-        return answer;
+        return result;
     }
 }
