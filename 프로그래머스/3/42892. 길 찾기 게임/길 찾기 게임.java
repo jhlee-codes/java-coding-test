071ac6cb44c73dadd3e844ddd19ca124f4d6e177
jihyun <jhleezz00@naver.com>
2026-01-27 00:36:21 +0900

    [level 3] Title: 길 찾기 게임, Time: 32.00 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,27 +1,28 @@
 import java.util.*;
 
 class Solution {
-    
-    private static class Node {
-        int x, y, num;
+        
+    public class Node {
+        int idx, x, y;
         Node left, right;
         
-        public Node(int num, int x, int y) {
-            this.num = num;
+        public Node(int idx, int x, int y) {
+            this.idx = idx;
             this.x = x;
             this.y = y;
         }
     }
-    
-    // 이진 트리 생성
-    private static Node makeBT(int[][] nodeinfo) {
-    
-        Node[] nodes = new Node[nodeinfo.length];
-        for (int i = 0; i < nodes.length; i++) {
+
+    public int[][] solution(int[][] nodeinfo) {
+                
+        int n = nodeinfo.length;
+        Node[] nodes = new Node[n];
+        
+        for (int i = 0; i < n; i++) {
             nodes[i] = new Node(i + 1, nodeinfo[i][0], nodeinfo[i][1]);
         }
         
-        // 정렬 (y 기준 내림차순, y가 같다면 x 기준 오름차순)
+        // 정렬 - 1. y 기준 내림차순 -> 2. x 기준 오름차순
         Arrays.sort(nodes, (o1, o2) -> {
             if (o1.y == o2.y) return Integer.compare(o1.x, o2.x);
             return Integer.compare(o2.y, o1.y);
@@ -29,61 +30,54 @@ class Solution {
         
         Node root = nodes[0];
         
+        // 트리 구성
         for (int i = 1; i < nodes.length; i++) {
             Node parent = root;
+            Node target = nodes[i];
             
             while (true) {
-                // 부모 노드의 x좌표가 더 크면 왼쪽으로
-                if (parent.x > nodes[i].x) {
+                if (parent.x > target.x) {
                     if (parent.left == null) {
-                        parent.left = nodes[i];
+                        parent.left = target;
                         break;
                     } else {
                         parent = parent.left;
                     }
-                }
-                // 부모 노드의 x좌표가 더 작거나 같으면 오른쪽으로
-                else {
+                } else {
                     if (parent.right == null) {
-                        parent.right = nodes[i];
+                        parent.right = target;
                         break;
-                    } else {
+                    } else { 
                         parent = parent.right;
                     }
                 }
             }
         }
         
-        return nodes[0];
-    }
-    
-    // 전위 순회
-    private static void preOrder(Node curr, ArrayList<Integer> answer) {
-        if (curr == null) return;
-        answer.add(curr.num);
-        preOrder(curr.left, answer);
-        preOrder(curr.right, answer);
-    }
-    
-    // 후위 순회
-    private static void postOrder(Node curr, ArrayList<Integer> answer) {
-        if (curr == null) return;
-        postOrder(curr.left, answer);
-        postOrder(curr.right, answer);
-        answer.add(curr.num);
-    }
-    
-    public int[][] solution(int[][] nodeinfo) {
+        List<Integer> preList = new ArrayList<>();
+        List<Integer> postList = new ArrayList<>();
         
-        Node root = makeBT(nodeinfo);
-        ArrayList<Integer> preOrderList = new ArrayList<>();
-        preOrder(root, preOrderList);
-        ArrayList<Integer> postOrderList = new ArrayList<>();
-        postOrder(root, postOrderList);
-    
-        int[][] answer = new int[2][nodeinfo.length];
-        answer[0] = preOrderList.stream().mapToInt(Integer::intValue).toArray();
-        answer[1] = postOrderList.stream().mapToInt(Integer::intValue).toArray();
+        preOrder(root, preList);
+        postOrder(root, postList);
+        
+        int[][] answer = new int[2][n];
+        answer[0] = preList.stream().mapToInt(Integer::intValue).toArray();
+        answer[1] = postList.stream().mapToInt(Integer::intValue).toArray();
+        
         return answer;
+    }
+    
+    private void preOrder(Node cur, List<Integer> answer) {
+        if (cur == null) return;
+        answer.add(cur.idx);
+        preOrder(cur.left, answer);
+        preOrder(cur.right, answer);
+    }
+    
+    private void postOrder(Node cur, List<Integer> answer) {
+        if (cur == null) return;
+        postOrder(cur.left, answer);
+        postOrder(cur.right, answer);
+        answer.add(cur.idx);
     }
 }
